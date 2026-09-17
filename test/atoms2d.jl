42da86b1c4bdcbd28b024ab8f27472db3a6140cf
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2026-09-17 13:31:31 +0200

    🍱 Update testing files

--- test/atoms2d.jl
+++ test/atoms2d.jl
@@ -1,3 +1,4 @@
 using NQCBase, NQCPlots, CairoMakie
 structure = structure = read_extxyz("test/Cu111_T-200K_3x3x6_highH_relax.xyz") |> first
-f, ax, pl = atoms2d(structure, axis=(title="Atoms2D Test", aspect=:data))
+f, ax, pl = atoms2d(structure, axis=(title="Atoms2D Test", aspect=DataAspect()))
+display(f)
